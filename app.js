a5d78f29d3cb3a4a4d41da616c4d2c08581f1f56
Andrew White <arlw87@googlemail.com>
2020-10-26 16:30:46 +0000

    Further developments

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
 const express = require('express');
-
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+
+//pug
+app.set('view engine', 'pug');
 
 //third party middleware
 app.use(morgan('dev'));
+app.set('views', path.join(__dirname, 'views'));
 
 //static images
 app.use(express.static(`${__dirname}/static/images`));
 
+//create an array of the images
+const imageFolder = path.join(__dirname, 'static', 'images');
+const images = fs.readdirSync(imageFolder);
+
 //routing
 app.get('/', (req, res) => {
-
-    res.end("This is a test");
-
+    res.status(200).render('base', {
+        author: 'Andrew',
+        message: 'Using Pug Locals'
+    });
 });
 
+app.get('/random', (req, res) => {
+    console.log(`serves a random image`);
+    randomNumber(images.length).then( randomNumber => {
+        console.log("Random Number in then: " + randomNumber);
+        let theImage = images[randomNumber];
+        res.status(200).render('random', { 
+            randomImage: theImage
+        });
+    }).catch(err => console.log(err));
+});
 
+app.get('/gallery', async (req, res) => {
+    console.log("Gallery Page");
+    res.status(200).render('gallery', {
+        images: images
+    });
+});
+
+//create an asyn function 
+async function randomNumber(total){
+    try{
+        var num = await Math.floor(Math.random() * total);
+        return num;
+    } catch (err){
+        console.log("There has been an error: " + err)
+        throw err;
+    }
+}
 
 module.exports = app;

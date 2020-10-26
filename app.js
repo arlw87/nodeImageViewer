ac84656b5008faebcb85c7873d560df8124eeb6b
Andrew White <arlw87@googlemail.com>
2020-10-26 15:08:42 +0000

    very basic server working

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//Testing one two
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+
+//third party middleware
+app.use(morgan('dev'));
+
+//static images
+app.use(express.static(`${__dirname}/static/images`));
+
+//routing
+app.get('/', (req, res) => {
+
+    res.end("This is a test");
+
+});
+
+
+
+module.exports = app;

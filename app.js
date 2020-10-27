81e5ab75b4055fc42b02023b9dbf129b120de2ee
Andrew White <arlw87@googlemail.com>
2020-10-27 13:37:15 +0000

    Improved file upload, image gallery, add homepage

--- app.js
+++ app.js
@@ -45,10 +45,7 @@ const writeThumbnail = async (image, imageFolderPath, imageThumbnailFolderPath) 
 
 //routing
 app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        author: 'Andrew',
-        message: 'Using Pug Locals'
-    });
+    res.status(200).render('home');
 });
 
 app.get('/random', (req, res) => {
@@ -64,6 +61,10 @@ app.get('/random', (req, res) => {
 
 app.get('/gallery', async (req, res) => {
     console.log(imageThumbnailFolder)
+    //need to check if there are more images then thumbsnails
+    //if there are you need to add more thumbnails 
+    //otherwise if image is sent via gmail or gdrive it show until server restarted
+
     //read in the images files and then render the page
     fs.readdir(imageThumbnailFolder, (err, files) => {
         console.log(files);
@@ -73,6 +74,11 @@ app.get('/gallery', async (req, res) => {
     });
 });
 
+app.get('/gallery/:imageDetail', (req, res) => {
+    const imageDetail = req.params
+    res.end("Finished");
+});
+
 app.get('/upload', (req, res) => {
     res.status(200).render('upload');
 });
@@ -80,26 +86,37 @@ app.get('/upload', (req, res) => {
 app.post('/upload', (req, res) => {
     console.log('image upload');
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were upload');
+        return uploadResponse(res, 400, "No file selected", "Try again?");
     }
 
     let imageUpload = req.files.imageToUpload;
 
+    if (imageUpload.mimetype !== 'image/jpeg'){
+        return uploadResponse(res, 500, "file was not a jpg image and therefore wasnt uploaded", "Try again?");
+    }
+
     imageUpload.mv(path.join(__dirname, '/static/images', imageUpload.name), (err) => {
         if (err){
-            return res.status(500).send(err);
+            return uploadResponse(500, `Error saving the file to the photoframe - ${err}`, "Try again?");
         }
         //needs to run after the image has saved inorder to make it a thumbnail
         writeThumbnail(imageUpload.name, imageFolder, imageThumbnailFolder);
     });   
+
+    uploadResponse(res, 200, "Upload was Successful", "Upload another?");
      
-    res.send('File Upload');
-    console.log("finished Upload function");
+    // res.status('200').render('uploadFinish', { 
+    //     status: "Upload Successful"
+    // });
 
 });
 
-
-
+const uploadResponse = (res, status, result, linkText) => {
+    res.status(status).render('uploadFinish', { 
+        status: result,
+        linkMessage: linkText
+    });
+};
 
 //create an asyn function 
 async function randomNumber(total){

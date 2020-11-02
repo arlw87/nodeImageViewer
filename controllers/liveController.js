c35de6f48998f015c7525a19f44534fab8546285
Andrew White <arlw87@googlemail.com>
2020-11-02 00:08:55 +0000

    Created Basic Live Slideshow

--- controllers/liveController.js
+++ controllers/liveController.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const commonFunctions = require('./../commonFunctions');
+var sizeOf = require('image-size');
 
 //FILE PATHS
-const imageFolder = path.join(__dirname, '..', 'static', 'images');
+const imageFolder = commonFunctions.getImageFolderPath();
 const imageThumbnailFolder = commonFunctions.getThumbnailImageFolderPath();
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//instead of refreshing the page you could use a ajax call 
+// to get the time interval and to sent the image
+// i think this would be neater.
 
 exports.liveView = (req, res) => {
     var now = new Date();
     console.log('Live Request');
-
-    console.log(imageThumbnailFolder);
+    console.log(imageFolder);
 
     //read in the images files and then render the page
-    fs.readdir(imageThumbnailFolder, (err, files) => {
+    fs.readdir(imageFolder, (err, files) => {
         console.log(files.length);
         var oneFile = files[random(files.length)];
         console.log(random(files.length));
+        // var dimensions = sizeOf(path.join(imageFolder, oneFile));
+        // console.log(dimensions.width, dimensions.height);
         res.status(200).render('live', {
             image: oneFile
         });
@@ -28,3 +35,38 @@ const random = (size) => {
     return Math.floor(Math.random() * size);
 
 };
+
+exports.update = (req, res) => {
+    console.log("Live update");
+    //respond with json data
+    //read in the images files and then render the page
+    fs.readdir(imageFolder, (err, files) => {
+        console.log(files.length);
+        var oneFile = files[random(files.length)];
+        console.log(random(files.length));
+        res.status(200).json({ 
+            status: 'success',
+            data: {
+                timeInterval: 20000,
+                imagePath: oneFile
+            }    
+        });
+    });  
+};
+
+
+// exports.updateImage = (req, res) => {
+
+//     console.log("Updating image");
+//     var options = { 
+//         root: commonFunctions.getImageFolderPath(), 
+//         headers: {
+//             'x-timestamp': Date.now(), 
+//             'x-sent': true, 
+//             'content-type': 'image/jpeg'
+//         }
+//     }
+
+//     var imageFiles = commonFunctions.get
+
+// }
--- routes/liveRoute.js
+++ routes/liveRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('./../controllers/liveController');
 
 router.route('/').get(controller.liveView);
+router.route('/update').get(controller.update);
 
 module.exports = router;
